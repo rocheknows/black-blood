5ee426cc8e973360bd81222c35bc49e02ea59eb8
rocheknows <tomrochee@gmail.com>
2020-06-18 11:23:04 +0100

    JS loader functionality improvements
    Previously changing {background: image;} now, changing image within the DOM. Images are loaded in the html with display: none; to improve speed.

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
-var backgroundImg = ["img/1.jpg", "img/2.jpg"]; //Add more backgrounds to the array
+setTimeout(function () {
+  $(".loader").fadeOut("slow");
+}, 4000);
+
+var backgroundImg = [
+  "img/1.jpg",
+  "img/2.jpg",
+  "img/3.jpg",
+  "img/5.jpg",
+  "img/4.jpg",
+  "img/6.jpg",
+  "img/7.jpg",
+  "img/8.jpg",
+  "img/9.jpg",
+  "img/10.jpg",
+]; //Add more backgrounds to the array
 var backgroundCount = 0;
 
 $(function () {
@@ -11,7 +26,10 @@ $(function () {
 $("#images").on("click", function () {
   backgroundCount++;
   if (backgroundCount > backgroundImg.length - 1)
-    $(".main").css("visibility", "visible");
+    $(".main").css("visibility", "visible"),
+      $("#images").css("display", "none");
+  $("body").css("backgroundColor", "#ffffff");
+
   $("#images").css(
     "background-image",
     "url(" + backgroundImg[backgroundCount] + ")"
